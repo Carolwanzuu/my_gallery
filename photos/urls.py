52bf8a93b4d9f884a759e2507d831e5c13ce0cc8
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-04 07:56:02 +0300

    defining urls

--- photos/urls.py
+++ photos/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.gallery,name = 'gallery'),
+    url(r'^photo/', views.viewPhoto, name='view_photo'),
+    url(r'^location/', views.locatePhoto, name='locate_photo'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -4,3 +4,12 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to my Gallery')
+
+def gallery(request):
+    return render(request,'gallery.html')
+
+def viewPhoto(request):
+    return render(request,'photo.html')
+
+def locatePhoto(request):
+    return render(request,'location.html')
