9995bc068d396f290bc0e9c1f24ddf588fe12071
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-04 18:00:54 +0300

    installing pillow

--- photos/models.py
+++ photos/models.py
@@ -5,10 +5,10 @@ class Image(models.Model):
     image = models.ImageField()
     image_name = models.CharField(max_length=30)
     image_description= models.CharField(max_length=100)
-    location= models.CharField(max_length=100)
+    # location= models.CharField(max_length=100)
     image_category=models.CharField(max_length=50)
 
-class Location(models.Model):
-    name=models.CharField(max_length=50)
-    image=models.ForeignKey(Image,on_delete=models.CASCADE,)
+# class Location(models.Model):
+#     name=models.CharField(max_length=50)
+#     image=models.ForeignKey(Image,on_delete=models.CASCADE,)
 
