ca517e29d34b98564a43bf40921392fdccdcacf7
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-05 04:37:02 +0300

    migrating new model

--- photos/models.py
+++ photos/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from django.db.models.fields.related import OneToOneField
 
 # Create your models here.
+class Category(models.Model):
+    name= models.CharField(max_length=100, null =False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Image(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(null=False,blank=False)
     image_name = models.CharField(max_length=30)
-    image_description= models.CharField(max_length=100)
+    image_description= models.TextField()
     # location= models.CharField(max_length=100)
-    image_category=models.CharField(max_length=50)
+    category=models.ForeignKey(Category, on_delete=models.SET_NULL, null=True )
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.image_name
 
 # class Location(models.Model):
 #     name=models.CharField(max_length=50)
